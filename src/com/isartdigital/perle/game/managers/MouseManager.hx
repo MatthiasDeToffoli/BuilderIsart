57daf6f4617e7dfccc8a24504a5c1244d06fd20e
dallenGD <dallen89@gmail.com>
2017-02-15 12:36:42 +0100

    added fonts & changed mouse manager value (with ambroise autorisation)

--- src/com/isartdigital/perle/game/managers/MouseManager.hx
+++ src/com/isartdigital/perle/game/managers/MouseManager.hx
@@ -107,7 +107,7 @@ class MouseManager {
 			moveGameContainer(positionInGame);
 	}
 	
-	public static inline var MAX_DURATION_FOR_SHORT_CLICK:Int = 420; // milliseconds
+	public static inline var MAX_DURATION_FOR_SHORT_CLICK:Int = 220; // milliseconds
 	private var mouseDownDuration:Int = 0; // milliseconds
 	
 	/**
