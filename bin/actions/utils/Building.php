4584cc093933f59047479d7f913c85a9b90e0b82
ambroiseRabier <rabier.ambroise@outlook.fr>
2017-02-26 00:11:31 +0100

    Added table Building and database table contant for php

---
+++ bin/actions/utils/Building.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: RABIERAmbroise
+ * Date: 25/02/2017
+ * Time: 21:20
+ */
+
+namespace actions\utils;
+
+abstract class Building {
+    const ID = "ID";
+    const IDTypeBuilding = "IDTypeBuilding";
+    const IDPlayer = "IDPlayer";
+    const StartConstruction = "StartConstruction";
+    const EndConstruction = "EndConstruction";
+    const NbResource = "NbResource";
+    const EndForNextProduction = "EndForNextProduction";
+    const NbSoul = "NbSoul";
+    const RegionX = "RegionX";
+    const RegionY = "RegionY";
+    const X = "X";
+    const Y = "Y";
+    const NbBuildingHeaven = "NbBuildingHeaven";
+    const NbBuildingHell = "NbBuildingHell";
+    const PackId = "PackId";
+}
---
+++ bin/actions/utils/Table.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: RABIERAmbroise
+ * Date: 25/02/2017
+ * Time: 20:19
+ */
+namespace actions\utils;
+
+abstract class Table {
+    const Building = "Building";
+    const Choices = "Choices";
+    const ChoicesUsed = "ChoicesUsed";
+    const Config = "Config";
+    const ConfigEvent = "ConfigEvent";
+    const Interns = "Interns";
+    const LevelReward = "LevelReward";
+    const Logs = "Logs";
+    const Player = "Player";
+    const PlayerInterns = "PlayerInterns";
+    const Region = "Region";
+    const Resources = "Resources";
+    const SoulAdjective = "SoulAdjective";
+    const SoulName = "SoulName";
+    const TimeConstruction = "TimeConstruction";
+    const TimeQuest = "TimeQuest";
+    const TypeBuilding = "TypeBuilding";
+    const TypePack = "TypePack";
+    const TypeShopPack = "TypeShopPack";
+}
