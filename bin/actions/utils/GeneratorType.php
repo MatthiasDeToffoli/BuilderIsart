90598c278b6b72489cb1200a10b370b0bcc531bf
ambroiseRabier <rabier.ambroise@outlook.fr>
2017-02-25 13:47:58 +0100

    Added: Constant class (like an enum) for table TypeBuilding and enum generatorType (from column Type in table Resources)

---
+++ bin/actions/utils/GeneratorType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: RABIERAmbroise
+ * Date: 25/02/2017
+ * Time: 11:32
+ */
+namespace actions\utils;
+
+abstract class GeneratorType
+{
+    const hard = "hard";
+    const soft = "soft";
+    const resourcesFromHell = "resourcesFromHell";
+    const resourcesFromHeaven = "resourcesFromHeaven";
+    const badXp = "badXp";
+    const goodXP = "goodXP";
+}
---
+++ bin/actions/utils/TypeBuilding.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: RABIERAmbroise
+ * Date: 25/02/2017
+ * Time: 11:37
+ */
+
+namespace actions\utils;
+
+abstract class TypeBuilding
+{
+    const ID = "ID";
+    const Name = "Name";
+    const Level = "Level";
+    const Alignment = "Alignment";
+    const Width = "Width";
+    const Height = "Height";
+    const FootPrint = "FootPrint";
+    const CostGold = "CostGold";
+    const CostWood = "CostWood";
+    const CostIron = "CostIron";
+    const CostKarma = "CostKarma";
+    const ConstructionTime = "ConstructionTime";
+    const ProductionType = "ProductionType";
+    const ProductionPerHour = "ProductionPerHour";
+    const ProductionResource = "ProductionResource";
+    const ProductionPerBuildingHeaven = "ProductionPerBuildingHeaven";
+    const ProductionPerBuildingHell = "ProductionPerBuildingHell";
+    const XPatCreationHeaven = "XPatCreationHeaven";
+    const XPatCreationHell = "XPatCreationHell";
+    const LevelUnlocked = "LevelUnlocked";
+    const LimitPerRegion = "LimitPerRegion";
+    const FactoryNeededToUnlock = "FactoryNeededToUnlock";
+    const MaxSoulsContained = "MaxSoulsContained";
+    const MaxGoldContained = "MaxGoldContained";
+    const IDPack1 = "IDPack1";
+    const IDPack2 = "IDPack2";
+    const IDPack3 = "IDPack3";
+    const IDPack4 = "IDPack4";
+    const IDPack5 = "IDPack5";
+    const IDPack6 = "IDPack6";
+}
