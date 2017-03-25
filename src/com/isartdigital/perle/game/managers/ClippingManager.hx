cca34f2ac6b8e1ba7b1d910f66d6a3cd306a39cf
ambroiseRabier <rabier.ambroise@outlook.fr>
2017-03-25 19:43:59 +0100

    Revert "Add: sort of background region with clipping"
    This reverts commit ab90fd8f8c66bb03b99cb8109b43559c9d97da13.

--- src/com/isartdigital/perle/game/managers/ClippingManager.hx
+++ src/com/isartdigital/perle/game/managers/ClippingManager.hx
@@ -92,7 +92,6 @@ class ClippingManager
 		if (needSortTiles) {
 			needSortTiles = false;
 			Building.sortBuildings();
-			RegionManager.sortBackground();
 			// todo : addregion here when they will be clipped.
 		}
 		
